1a431b238cc5d9e522c54e3b06f95d664c539a38
adiaz0824 <adiaz0824@gmail.com>
2015-01-22 22:00:56 -0800

    changed % to px

---
+++ tictactoe.js
@@ -0,0 +1,39 @@
+// ========================================================
+// TIC TAC IRONMAN JS PSEUDOCODE 
+// ========================================================
+// -We will define two variables for "Player 1" and "Player 2"
+// var Player1 and var Player2
+// Player1 and Player2 are empty arrays:
+var Player1 = [];
+var Player2 = [];
+
+// -We will have to establish which player will go first
+// by creating an onlick event where the users can choose player 1 or 2
+
+// -Once the first Player is chosen a script will run to alternate 
+// between players
+
+// -For every time a player hovers over each game tile, that tile 
+// will change colors to display that the mouse is over that tile
+// thus using a "Hover" or "Mouseover" event
+
+// -When a player clicks on a <div> tile, there will be an 
+// "onclick" event that will turn the <div> into a picture or 
+// an X or O corresponding to Player1 or Player2 
+
+// -There will be another function inside the onclick event which
+// will push the <div> into a Player1 and Player2 array
+
+// -We will need to create a winning array combination based
+// on the game tile <divs>
+
+// -We will need to create a loop that checks the values or 
+// Player1 and Player2's array and see if they match the winning array
+
+// -If there is a match, then a message will appear to state either
+// Player1 or Player2 has won.
+
+// -If you click the reset button, then the game will reset
+
+
+
